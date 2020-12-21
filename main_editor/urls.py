29441768b3b787c4ba6ce1108f8df0b33e96f67e
alex <baki.alexey@yandex.ru>
2020-12-21 14:41:27 +0300

    pagination

--- main_editor/urls.py
+++ main_editor/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.SearchResultsView.as_view(), name='search'),
     path(r'delete-image/(<id>\d+)/', views.delete_image, name="delete_image"),
-]
+    path('image_detail/<int:id>/', views.detail_image, name='image_detail')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main_editor/views.py
+++ main_editor/views.py
@@ -12,9 +12,8 @@ def index(request):
     articles = ArticleDetail.objects.all()
 
     page_number = request.GET.get('page')
-    index_count = 2
-    if request.GET.get('limit'):
-        index_count = int(request.GET.get('limit'))
+    limit = request.GET.get('limit')
+    index_count = int(request.GET.get('limit'))
 
     paginator = Paginator(articles, index_count)
 
@@ -28,13 +27,15 @@ def index(request):
     is_paginated = pages.has_other_pages()
 
     content = {'pages': pages,
-               'is_paginated': is_paginated}
+               'is_paginated': is_paginated,
+               'limit': limit}
     return render(request, 'main_editor/block.html', content)
 
 
 class SearchResultsView(ListView):
     model = ArticleDetail
     template_name = 'main_editor/search.html'
+    context_object_name = 'article_object'
     paginate_by = 2
 
 
@@ -50,9 +51,26 @@ class SearchResultsView(ListView):
         context['searchq'] = q
         return context
 
+# попробовать реализовать через класс
+"""from django.views.generic.edit import DeleteView
 
+class ItemDelete(DeleteView):
+    model = Item
+    template_name = 'item_confirm_delete.html'
+    success_url = '/success/'
+    # по ключу передать параметр кнопки удаления 
+    def get_context_data(self, **kwargs):
+        context = super(SearchResultsView, self).get_context_data(**kwargs)
+        q = self.request.GET.get('q')
+        context['searchq'] = q
+        return context
+"""
 def delete_image(request, id):
     image =ImageArticle.objects.get(pk=id).delete()
     host = request.META.get('HTTP_REFERER')
     return redirect(host)
+
+def detail_image(request, id):
+    image_detail = ImageArticle.objects.get(pk=id)
+    return render(request, 'main_editor/index.html', {'image_detail': image_detail})
 
